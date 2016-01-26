aed9c0e06d47795e8ac1076beb26a501de525579
ynonperek <ynonperek@gmail.com>
2016-01-26 10:00:00 +0200

    tic tac toe game demo

---
+++ 06-tictactoe/game.js
@@ -0,0 +1,69 @@
+var game = document.querySelector('.game');
+var currentPlayer = 'X';
+var gameOver = false;
+
+function handleClick(ev) {
+  var sqr = ev.target;
+
+  if (gameOver) { return; }
+
+  if (isBlank(sqr)) {
+    sqr.textContent = currentPlayer;
+    var result = hasWinner(currentPlayer);
+    if (result) {
+      alert('Wow You Win: ' + result.player);
+      var squares = document.querySelectorAll('.square');      
+      for( var index of result.indexes ) {
+        squares[index].classList.add('winner');
+      }
+      gameOver = true;
+    }
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  }
+}
+
+function isBlank(el) {
+  return el.textContent === " ";
+}
+
+function hasWinner(player) {
+  var board = game.textContent.split("");
+  if ( (board[0] === player) && (board[1] === player) && (board[2] === player) ) {
+    return { player: player, indexes: [0, 1, 2] };
+  }
+  if ( (board[3] === player) && (board[4] === player) && (board[5] === player) ) {
+    return { player: player, indexes: [3, 4, 5] };
+  }
+  if ( (board[6] === player) && (board[7] === player) && (board[8] === player) ) {
+    return { player: player, indexes: [6, 7, 8] };
+  }
+  if ( (board[0] === player) && (board[3] === player) && (board[6] === player) ) {
+    return { player: player, indexes: [0, 3, 6] };
+  }
+  if ( (board[1] === player) && (board[4] === player) && (board[7] === player) ) {
+    return { player: player, indexes: [1, 4, 7] };
+  }
+  if ( (board[2] === player) && (board[5] === player) && (board[8] === player) ) {
+    return { player: player, indexes: [2, 5, 8] };
+  }
+  if ( (board[0] === player) && (board[4] === player) && (board[8] === player) ) {
+    return { player: player, indexes: [0, 4, 8] };
+  }
+  if ( (board[2] === player) && (board[4] === player) && (board[6] === player) ) {
+    return { player: player, indexes: [2, 4, 6] };
+  }
+  return null;
+}
+
+for (var i=0; i < 3; i++ ) {
+  var row = document.createElement('div');
+  row.classList.add('row');
+  
+  for ( var j=0; j<3; j++ ) {
+    row.innerHTML += '<div class="square"> </div>';
+  }
+
+  game.appendChild(row);
+}
+
+game.addEventListener('click', handleClick);
