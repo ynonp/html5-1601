2795f4850298d219416a04c724414a77644b515d
ynonperek <ynonperek@gmail.com>
2016-01-26 11:53:36 +0200

    network example

--- 07-network/omdb.js
+++ 07-network/omdb.js
@@ -1,7 +1,20 @@
 var btn = document.querySelector('#btn-search');
 var inp = document.querySelector('#i-name');
+var wait = document.querySelector('.please-wait');
+var form = document.querySelector('form');
+
+var target = document.getElementById('spinner');
+var spinner = new Spinner().spin(target);
+
+function handleSubmit(ev) {
+  search(inp.value);
+  ev.preventDefault();
+}
+
 
 function search(movieName) {
+  wait.classList.add('show');
+
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'http://omdbapi.com/?s=' + movieName);
   xhr.addEventListener('load', handleLoad);
@@ -9,6 +22,8 @@ function search(movieName) {
 }
 
 function showMovie(movieData) {
+  wait.classList.remove('show');
+
   document.querySelector('.movie-name').innerHTML = movieData.Title;
   document.querySelector('.movie-year').innerHTML = movieData.Year;
   document.querySelector('.movie-poster').src = movieData.Poster;
@@ -30,4 +45,14 @@ function handleSearchClick(ev) {
   search(inp.value);
 }
 
-btn.addEventListener('click', handleSearchClick);
+form.addEventListener('submit', handleSubmit);
+
+
+
+
+
+
+
+
+
+
